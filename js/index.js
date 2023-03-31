71df260624bbf21532150e110dc9c38c7f9ce073
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-31 15:08:57 -0300

    Calculo do consumo da gasolina concluído

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ function pegaValores() {
 	var campoValorDistancia = document.getElementById("distancia");
 	var campoValorLitros = document.getElementById("litros");
 
+	var resposta = document.getElementById("respostaConsumoGasolina");
+
 	var distancia = campoValorDistancia.value;
 	var litros = campoValorLitros.value;
+
+	calculaConsumo(distancia, litros, resposta);
+}
+
+function calculaConsumo(distancia, litros, resposta) {
+	var consumoGasolina = distancia / litros;
+
+	resposta.textContent = "O consumo da gasolina foi de " + consumoGasolina + " litros.";
 }
