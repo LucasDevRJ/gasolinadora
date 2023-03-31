b89b78c523051a59707f6f5634557001daacc74f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-03-31 14:57:45 -0300

    Pegandos os campos de entrada

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 function pegaValores() {
 	var campoValorDistancia = document.getElementById("distancia");
+	var campoValorLitros = document.getElementById("litros");
+
+	
 
 	console.log(campoValorDistancia);
 }
